895b797b375f06ab94e431b7896e1c86aadf23c1
Johnny.Yan <1058012452@qq.com>
2017-07-05 17:44:52 +0800

    add wechat module

--- trans.py
+++ trans.py
@@ -1,16 +1,16 @@
-
 import os,sys,ftplib,socket
 import datetime
 from logger import logging
 #from wechat import wechat
 
-FILENAME = "T"+datetime.datetime.now().strftime('%Y%m%d')+"gz"
+FILENAME = "T"+datetime.datetime.now().strftime('%Y%m%d')+".gz"
 CONST_BUFFER_SIZE = 8192
 
-def connect(host, username, pwd):
+def connect(host, username, pwd,path):
     try:
         ftp = ftplib.FTP(host)
         ftp.login(username, pwd)
+        ftp.cwd(path)
         return ftp
     except socket.error, socket.gaierror:
         logging.error("FTP is unavailable")
@@ -20,52 +20,60 @@ def disconnect(ftp):
     ftp.quit()
     logging.info("ftp disconnect")
 
-def upload(ftp,filepath):
-    f = open(filepath, "rb")
-    file_name = os.path.split(filepath)[-1]
+def upload(ftp,filename):
+    path = os.path.abspath(".")
+    print path
+    f = open(path+"\\"+filename, "r")
     try:
-        ftp.storbinary('STOR %s' % file_name, f, CONST_BUFFER_SIZE)
+        ftp.storbinary('STOR %s' % filename, f, CONST_BUFFER_SIZE)
+        return True
     except ftplib.error_perm:
         logging.error("upload file error")
-    logging.info("upload file success")
+        return False
 
 def download(ftp,filename):
-    f = open(filename,"wb").write
+    f = open(filename, "wb").write
     try:
         ftp.retrbinary("RETR %s" % filename, f, CONST_BUFFER_SIZE)
+        return True
     except ftplib.error_proto:
-        logging.error("download %s failed"+filename)
-    logging.info("download %s success" +filename)
+        logging.error("download %s failed" %filename)
+        return False
 
 def checkfile(ftp,filename):
     ftp_f_list = ftp.nlst()
     if filename in ftp_f_list:
-        logging.info("file exzit")
+        logging.info("file exist")
         return True
     else:
         logging.error("no files")
         return False
 
 def main():
-    ftplocal = connect("58.247.98.30", "ftpuser", "MeiyouMima123")
-    logging.info("begin to download file to local path")
-    if checkfile(ftplocal, FILENAME):
+    ftpsodex = connect("192.168.1.8", "ftpuser", "MeiyouMima123", "trans")
+    logging.info("begin to download kfc_trans file")
+    if checkfile(ftpsodex, FILENAME):
         pass
     else:
-        disconnect(ftplocal)
+        disconnect(ftpsodex)
         sys.exit(0)
-    if download(ftplocal, FILENAME):
+    if download(ftpsodex, FILENAME):
         pass
     else:
+        logging.error("error")
+        disconnect(ftpsodex)
         sys.exit(0)
-    disconnect(ftplocal)
+    #disconnect(ftpsodex)
     logging.info("Download local file  finished")
 
-    ftpremote = connect("58.247.98.30", "ftpuser", "MeiyouMima123")
+    ftpremote = connect("192.168.1.8", "ftpuser", "MeiyouMima123", "blacklist")
     logging.info("begin to upload file to remote path")
     if upload(ftpremote, FILENAME):
+        logging.info("upload trans file successful")
         pass
     else:
+        logging.error("upload trans file failed")
+        disconnect(ftpremote)
         sys.exit(0)
     disconnect(ftpremote)
     #wechat.sendwechat("johnnyjh")
--- wechat.py
+++ wechat.py
@@ -1,50 +1,49 @@
 #!/usr/bin/env python
 #coding=utf-8
-
-import urllib2
-import simplejson as json
-import sys
-import requests
-import time
+import os,urllib2,requests,sys,time,json,pickle
 
 corpid = 'wx15565f83aa3c66b8'
-secret = 'SP3IIAU-69WIIcq3LULKgC_QzZnV71k9wnP-0Xp5jla13TEJkQQja2nNDTsIsBa3'
-class wechat:
-    def __init__(self, corpid, secret ):
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s' %  (corpid ,secret)
-        self.expire_time = sys.maxint
-        self.access_token = get_token()
+secret = 'pbxYUirUS3OfZJ48Rwp5PPaC5h4a-SXkPhOO1hC-E80'
 
-    def get_token(self):
-        if self.expire_time > time.time():
-            request = urllib2.Request(self.baseurl)
-            response  = urllib2.urlopen(request)
-            ret = response.read().strip()
-            ret = json.loads(ret)
-            if 'errcode' in ret.keys():
-                print >> ret['errmsg'], sys.stderr
-                sys.exit(1)
-            self.expire_time = time.time() + ret['expires_in']
-            self.access_token = ret['access_token']
+class Token(object):
+  """Get AccessToken"""
+  def __init__(self, corpid, secret):
+    self.baseurl = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={0}&corpsecret={1}'.format(corpid, secret)
+    self.expire_time = sys.maxint
+
+  def get_token(self):
+    if self.expire_time > time.time():
+      request = urllib2.Request(self.baseurl)
+      response = urllib2.urlopen(request)
+      ret = response.read().strip()
+      ret = json.loads(ret)
+      print ret
+      if 'errcode' in ret.keys():
+        #return True
+        #print  ret
+
+        self.expire_time = time.time() + ret['expires_in']
+        self.access_token = ret['access_token']
+
         return self.access_token
+      else:
+        print "error"
 
-    def sendwechat(self, user, title, content):
-        url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={0}".format()
-        varload = {
-            "touser": "{0}".format(user),
-            "msgtype": "text",
-            "agentid": "1",
-            "text": {
-                "content": "{0}\n{1}".format(title, content)
-            },
-            "safe": "0"
-        }
-        ret = requests.post(url, data=json.dumps(varload, ensure_ascii=False))
-        print ret.json()
-
-
-
+def SendMsg(user, title, content):
+  gettoken = Token(corpid=corpid, secret=secret).get_token()
+  print gettoken
+  url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={0}".format(gettoken)
+  varload = {
+    "touser":"{0}".format(user),
+    "msgtype":"text",
+    "agentid":"1",
+    "text":{
+      "content":"{0}\n{1}".format(title, content)
+    },
+    "safe":"0"
+  }
+  ret = requests.post(url, data=json.dumps(varload, ensure_ascii=False))
+  print ret.json()
 if __name__ == '__main__':
-    wechat = wechat(corpid, secret)
-#    wechat.sendwechat(sys.argv[1], sys.argv[2], sys.argv[3])
-    wechat.sendwechat("johnny","KFC","test")
+#SendMsg(sys.argv[1],sys.argv[2],sys.argv[3])
+    SendMsg("yanchengjh", "TEST", "test")
